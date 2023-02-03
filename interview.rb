3e0a912275426f0ad4998d750f8cb5a96e12d0ff
daikiweb1221 <daiki.web1221@gmail.com>
2023-02-03 21:26:02 +0900

    Add: firstcommit

---
+++ interview.rb
@@ -0,0 +1,160 @@
+# number_array = [3, 5, 9, 7, 1]
+# size = number_array.size
+
+# size.times do |i|
+#   (size - (i + 1)).times do |j|
+#     if number_array[j] > number_array[j + 1]
+#       tmp = number_array[j]
+#       number_array[j] = number_array[j + 1]
+#       number_array[j + 1] = tmp
+#     end
+#   end
+# end
+
+# p number_array
+
+# money = 6498
+# money_types = [10000, 5000, 1000, 500, 100, 50, 10, 5, 1]
+# total = 0
+# result= []
+# money_types.each do |type|
+#   amount = (money - total) / type
+#   total += type * amount
+#   result.push([type, amount])
+# end
+
+# p result.reverse.to_h
+
+
+# array_data = [["らんてくん", 98],["カンノ", 23],["ファラオ", 57],["だいそん", 84],["ひさじゅ", 100]]
+# size = array_data.size
+# array_data = array_data.map{|i| i.reverse }
+# result = array_data.sort.reverse
+# result.each_with_index do |obj, index|
+#   p "#{index + 1}位: #{obj[1]} #{obj[0]}点"
+# end
+
+
+# Rails実践カリキュラム基礎編の課題内容の学習時間をグループ分けして出力してください。
+# curriculum_dataは["課題項目", "学習時間"]で配列になっています。
+curriculum_data = [
+  ["課題1 アプリの基本設定を行う", 2],
+  ["課題2 全ページにヘッダー/フッターを設置", 6],
+  ["課題3 Gemを使ってみよう (Bootstrap)", 2],
+  ["課題4 sorceryを使用して、ユーザー機能を作成しよう", 16],
+  ["Sorcery課題", 4],
+  ["課題5 i18nによる日本語化対応", 4],
+  ["課題6 フラッシュメッセージの設定", 6],
+  ["課題7 デコレーターの導入", 4],
+  ["課題8 掲示板の一覧機能の作成", 8],
+  ["課題9 掲示板作成機能", 8],
+  ["課題10 フォーム入力時エラー情報を個別表示", 4],
+  ["BugFix課題", 3],
+  ["課題11 掲示板の画像アップロード機能", 12],
+  ["課題12 掲示板詳細画面の追加/コメント機能の実装", 16],
+  ["課題13 タイトルを動的に出力する", 4],
+  ["課題14 掲示板の編集、削除機能の実装", 8],
+  ["課題15 ブックマーク機能の追加", 12],
+  ["課題16 ブックマークボタンのajax化", 8],
+  ["Like課題", 4],
+  ["課題17 コメント投稿、削除、編集機能のajax化" ,12],
+  ["課題18 掲示板のページネーション", 8],
+  ["課題19 掲示板の検索機能を実装", 8],
+  ["課題20 プロフィール編集機能の実装", 4],
+  ["Profile課題", 4],
+  ["課題21 パスワードリセット機能の実装", 16],
+  ["課題22 [管理画面] 管理画面へのログイン機能、管理画面トップページの作成", 12],
+  ["課題23 [管理画面]掲示板/ユーザのCRUD機能の作成", 8],
+  ["Admin課題", 4]
+]
+
+# res = curriculum_data.group_by{|cd| cd[1]}.sort{|a, b| b[0] <=> a[0]}.map do |c|
+#   {hour: c[0], count: c[1].count, contents: c[1].map{|con| con[0]}}
+# end
+# p res
+# p  curriculum_data.group_by{|cd| cd[1]}.sort{|a, b| b[0] <=> a[0]}
+# p res = curriculum_data.group_by{|cd| cd[1]}
+# res.sort{|a, b| }
+
+# array = [2,5,6,8,1]
+# array.sort{|a, b|  p a,b }
+
+# arg = 'QETNUR'
+# size = arg.size
+# result = []
+# j = 0
+# while j < size do
+#   result.push(arg[5 - j])
+#   j += 1
+# end
+
+# p result.join
+
+# arg = 'あらるんごてりくずんむ'
+# arg = arg.split('')
+
+# odd = arg.select.each.with_index {|_, index| index.odd?}
+# even = arg.select.with_index {|_, index| index.even?}
+# p even.join + odd.join
+
+
+# arg = "Things may come to those who wait, but only the things left by those who hustle."
+# arg.delete!(',')
+# arg.delete!('.')
+# arg = arg.split(' ')
+
+# p arg.map{|i| i.length}
+
+# p gets.split.map(&:to_i).max
+
+# array = [[1,2], [3,2], [5,6], [2,6]]
+
+# hash = array.group_by{|i| i[1]}
+# p hash.sort{|a, b| a[0] <=> b[0]}
+
+# arg = 'あらるんごてりくずんむ'
+# arg_array =  arg.split("")
+# odd = []
+# even = []
+# arg_array.each_with_index do |value, index|
+#   if index % 2 == 0
+#     odd.push(value)
+#   else
+#     even.push(value)
+#   end
+# end
+
+# p odd.join + even.join
+
+
+# i = 1
+
+# while 101 > i do
+#   if i % 15 == 0
+#     puts 'FizzBuzz'
+#   elsif i % 3 == 0
+#     puts 'Fizz'
+#   elsif i % 5 == 0
+#     puts 'Buzz'
+#   else
+#     puts i.to_i
+#   end
+#   i += 1
+# end
+
+# arg = "Things may come to those who wait, but only the things left by those who hustle."
+# arg.delete!(',')
+# arg.delete!('.')
+# new_arg = arg.split(" ").map {|i| i.length}
+# p new_arg
+
+# array_data = [["らんてくん", 98],["カンノ", 23],["ファラオ", 57],["だいそん", 84],["ひさじゅ", 100]]
+# new_array = []
+# array_data.map do |name, points|
+#   new_array.push([points, name])
+# end
+
+# new_array.sort.reverse.each_with_index do  |value, index|
+#   puts "#{index + 1}位: #{value[1]}さん #{value[0]}点"
+# end
+
---
+++ interview2.rb
@@ -0,0 +1,605 @@
+# def fizz_buzz(n)
+#   1.upto(n) do |i|
+#     if i % 15 == 0
+#       puts 'FizzBuzz'
+#     elsif i % 3 == 0
+#       puts 'Fizz'
+#     elsif i % 5 == 0
+#       puts 'Buzz'
+#     else
+#       puts i.to_i
+#     end
+#   end
+# end
+
+# fizz_buzz(ARGV[0].to_i)
+
+# require 'net/http'
+# require 'uri'
+# require "json"
+# res =  Net::HTTP.get(URI.parse('https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json'))
+# p new_res  = JSON.parse(res)
+
+# puts "発表者: #{new_res["publishingOffice"]}"
+# puts "報告日時: #{new_res["reportDatetime"]}"
+# puts "対象地域: #{new_res["targetArea"]}"
+# puts "ヘッドライン: #{new_res["headlineText"]}"
+# puts "詳細: #{new_res["text"]}"
+
+# array = gets.split.map(&:to_i)
+# p array.sum / array.length
+
+# arg = gets.split
+# p arg
+
+# val = gets.chomp.split
+# p val
+
+# line = readlines.map(&:chomp)
+# p line
+
+# s = gets.chomp
+# n, m = gets.split.map(&:to_i)
+
+# answer = s
+# tmp = answer[n - 1]
+# answer[n - 1] = answer[m - 1]
+# answer[m - 1] = tmp
+
+# puts answer
+
+# n = gets.to_i
+# s = gets.split.map(&:chomp)
+
+# s = s.join
+
+# if s[n - 1] == 'o'
+#   puts '晴れ'
+# elsif s[n - 1] == 'x'
+#   puts '雨'
+# else
+#   puts 'oまたはxを入力してください'
+# end
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+
+# puts a.inject(1){|result, num| result *= num }
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+
+# new_a = a.map{|i| i % 10}
+# puts new_a
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i).select {|i| i % 3 == 0}
+
+# puts a
+# puts  gets.split.map(&:to_i).reverse
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+# size = a.length
+# puts a.sum / size
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+
+# new_a = a.sort
+# puts new_a[0]
+# n = gets.to_i
+# a = readlines.map(&:chomp)
+# puts a
+
+# n = gets.to_i
+# s = readlines.map(&:chomp)
+# p s.map{|i| i[0]}.join
+
+# array = gets.split.map(&:to_i)
+# size = array.size
+
+# size.times do
+#   (size - 1 ).times do |i|
+#     if  array[i] > array[i + 1]
+#     tmp = array[i]
+#     array[i] = array[i + 1]
+#     array[i + 1] = tmp
+#     end
+#   end
+# end
+
+# p array
+
+# n = gets.to_i
+# s = readlines.map(&:chomp)
+# right = []
+# left = []
+# s.select do |i|
+#   if i.include?('right')
+#     right.push(i)
+#   elsif i.include?('left')
+#     left.push(i)
+#   end
+# end
+
+# if right.length > left.length
+#   puts 'right'
+# elsif right.length < left.length
+#   puts 'left'
+# elsif right.length == left.length
+#   puts 'same'
+# end
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+
+# n.times do |i|
+#   sorted = false
+#   (n - 1).times do |j|
+#     if a[j] > a[j + 1]
+#       tmp = a[j]
+#       a[j] = a[j + 1]
+#       a[j + 1] = tmp
+#       sorted = true
+#     end
+#   end
+#   if sorted
+#     puts a.join(' ')
+#   else
+#     break
+#   end
+# end
+
+# array_data = [["らんてくん", 98],["カンノ", 23],["ファラオ", 57],["だいそん", 84],["ひさじゅ", 100]]
+
+# new_array = array_data.sort{|a, b| a[1] <=> b[1]}.reverse.to_h
+
+# new_array.each_with_index do |(name,points),index|
+#   puts "#{index + 1}位: #{name}さん => #{points}点"
+# end
+
+# n,v = gets.split.map(&:to_i)
+# a = gets.split.map(&:to_i)
+
+# if a.include?(v)
+#   puts 'Yes'
+# else
+#   puts 'No'
+# end
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+
+# a.size.times do |i|
+#   tmp = a.select {|j| a[i] > j}
+
+# end
+
+# n,v = gets.split.map(&:to_i)
+# a = gets.split.map(&:to_i)
+
+# puts a.count(v)
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+# count = 0
+
+# a.select{|i| 0 < i ? count += 1: count}
+# puts count
+
+# n,v = gets.split.map(&:to_i)
+# a = gets.split.map(&:to_i)
+# target = []
+# a.each_with_index do |val, index|
+#   if val == v
+#     target.push(index)
+#   end
+# end
+
+# if target.empty?
+#   puts '-1'
+# else
+#   puts target.max
+# end
+
+# int = 7777777
+# result = []
+# 7.upto(int) do |i|
+#   if i % 7 == 0
+#     result.push(i)
+#   end
+# end
+# ans =  result.map{|i| i.to_s.count('7')}
+# puts ans.sum
+
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+# result = 0
+# num = n - 1
+
+# (1..num).each do |i|
+#   if a[i - 1] < a[i]
+#     result += 1
+#   end
+# end
+# puts result
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+# size = a.length
+# num = n - 1
+
+# size.times do |i|
+#   num.times do |j|
+#     if a[j] < a[j + 1]
+#       tmp = a[j]
+#       a[j] = a[j + 1]
+#       a[j + 1] = tmp
+#     end
+#   end
+# end
+
+# puts a[0]
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+
+# a.each_with_index do |val, index|
+#   if val == a.max
+#     puts index
+#   end
+# end
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+
+# result = []
+
+# 100.upto(200) do |i|
+#   if i % 10 == 0
+#     result.push(i)
+#   end
+# end
+# puts result.length
+
+# sum = 0
+# for i in (1..100) do
+#   sum += i
+# end
+
+# puts sum
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+# array = [0] * 9
+
+# a.each do |x|
+#   array[x - 1] += 1
+# end
+
+# puts array
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+# array = Array.new(9,0)
+
+# a.each do |x|
+#   array[x - 1] += 1
+# end
+# puts array.index(array.max) + 1
+
+# a = gets.to_i
+# b,c = gets.split.map(&:to_i)
+# s = gets.to_s
+
+# puts a
+# puts "#{b} #{c}"
+# puts s
+# n = gets.to_i
+# s = gets.chomp
+# s.gsub!("na", "nya")
+# puts
+
+# money = gets.to_i
+# moneys = [1,5,10,50,100,500,1000,5000,10000]
+# total = 0
+# result= []
+
+# moneys.sort.reverse.each do |i|
+#   amount = (money - total) / i
+#   total += i * amount
+#   result.push([i, amount])
+# end
+
+# p result.reverse.to_h
+
+# str = 'QETNUR'
+# result = []
+# i = 0
+# j = -1
+
+# while i < str.size
+#   result.push(str[j])
+#   i += 1
+#   j -= 1
+# end
+
+# p result
+
+# str = "Things may come to those who wait, but only the things left by those who hustle."
+# str.delete!(',.')
+# p str.split(' ').map{|i| i.length}
+
+# str = 'あらるんごてりくずんむ'
+# odd = []
+# even = []
+
+# str.split('').each_with_index do |val,index|
+#   if index == 0
+#     even.push(val)
+#   elsif index % 2 == 0
+#       even.push(val)
+#   else
+#     odd.push(val)
+#   end
+# end
+# p even.join + odd.join
+
+# a,b = gets.split.map(&:to_i)
+# array = (1..b).to_a
+# result = []
+
+# array.each do |i|
+#   if a % i == 0 && b % i == 0
+#     result.push(i)
+#   end
+# end
+
+# puts result.max
+
+# n = gets.to_i
+# 1.upto(n) do |i|
+#   if i % 15 == 0
+#     puts 'FizzBuzz'
+#   elsif i % 3 == 0
+#     puts 'Fizz'
+#   elsif i % 5 == 0
+#     puts 'Buzz'
+#   else
+#     puts i.to_i
+#   end
+# end
+
+# require 'prime'
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+# result = []
+
+# a.each do |i|
+#   if Prime.prime?(i)
+#     result.push(i)
+#   end
+# end
+# puts result.length
+
+# result = []
+
+# (7..7777777).to_a.each do |i|
+#   if i % 7 == 0
+#     result.push(i)
+#   end
+# end
+# puts result.sum{|s| s.to_s.count('7')}
+
+# a, b = gets.split.map(&:chomp)
+
+# if a == 'H' && b == 'H'
+#   puts 'H'
+# elsif a == 'D' && b == 'D'
+#   puts 'H'
+# elsif a == 'D' && b == 'H'
+#   puts 'D'
+# elsif a == 'H' && b == 'D'
+#   puts 'H'
+# end
+
+# s = gets.chomp
+# size = s.length
+# count = 0
+
+# for i in (0...size)
+#   if s[i] == s[i + 1]
+#     count += 1
+#   end
+# end
+
+# puts count
+
+# n = gets.to_i
+# s = gets.chomp
+# t = gets.chomp
+# count = 0
+
+# n.times do |i|
+#   if s[i] != t[i]
+#     count += 1
+#   end
+# end
+
+# puts count
+
+# s = gets.chomp
+
+# puts s == s.reverse ? 'Yes': 'No'
+
+# n,k = gets.split.map(&:to_i)
+# array = (1..n).to_a
+# result = []
+
+# array.each do |i|
+#   count = 0
+#   (1..i).to_a.each do |j|
+#     if i % j == 0
+#       count += 1
+#     end
+#   end
+#   if count == k
+#     result.push(i)
+#   end
+# end
+
+# puts result.length
+
+# l,r = gets.split.map(&:to_i)
+# array = (l..r).to_a
+# count = 0
+
+# array.each do |i|
+#   if i.to_s == i.to_s.reverse
+#     count += 1
+#   end
+# end
+
+# puts count
+
+# s = gets.chomp
+# array = s.split('')
+
+# puts array.uniq.length
+
+# n = gets.to_i
+# s = readlines.map(&:chomp)
+# result = []
+
+# s.each do |str|
+#   if str == str.reverse
+#     result.push(str)
+#   end
+# end
+
+# puts result.length
+
+# x,y,z = gets.split.map(&:to_i)
+# a = gets.split.map(&:to_i)
+# b = gets.split.map(&:to_i)
+# c = gets.split.map(&:to_i)
+# count = 0
+
+# a.each do |i|
+#   b.each do |j|
+#     c.each do |k|
+#       if (i + j) == k
+#         count += 1
+#       end
+#     end
+#   end
+# end
+
+# puts count
+
+# n, k = gets.split.map(&:to_i)
+# a = gets.split.map(&:to_i)
+# count = 0
+
+# (0..n).each do |i|
+#   ((i + 1)...n).each do |j|
+#     if (a[i] + a[j]) <= k
+#       count += 1
+#     end
+#   end
+# end
+
+# puts count
+
+# l,r = gets.split.map(&:to_i)
+# count = 0
+# (l..r).each do |i|
+#   ((i + 1)..r).each do |j|
+#     if (i % 10) == (j % 10)
+#       count += 1
+#     end
+#   end
+# end
+
+# puts count
+
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+# count = 0
+
+# (0...n).each do |i|
+#   ((i+ 1)...n).each do |j|
+#     ((j + 1)...n).each do |k|
+#       if [a[i], a[j], a[k]].max == a[j]
+#         count += 1
+#       end
+#     end
+#   end
+# end
+
+# puts count
+
+# n = gets.to_i
+# s = readlines.map(&:chomp)
+# str = []
+
+# (0...n).each do |i|
+#   ((i + 1)...n).each do |j|
+#     s[i] == s[j] ? str << 'Yes': str << ''
+#   end
+# end
+
+# if  str.join('').include?('Yes')
+#   puts 'Yes'
+# else
+#   puts 'No'
+# end
+
+# n = gets.to_i
+# s = gets.chomp
+# s_array = s.split('')
+# count = 0
+
+# (0...n).each do |i|
+#   ((i + 1)...n).each do |j|
+#     if s_array[i] == s_array[j]
+#       count += 1
+#     end
+#   end
+# end
+
+# puts count
+# i = 42
+# i = i * 0.8
+# i = i.floor
+# i = i * 0.8
+# i = i.floor
+# i = i * 0.8
+# i = i.floor
+# p i
+
+array = (1..101).to_a
+result = []
+array.each do |i|
+  if i % 2 != 0
+    result << i
+  end
+end
+
+ans = []
+result.each do |j|
+  ans <<  j ** 3
+end
+
+p ans.sum
+
+# result = []
+
+# (1..333333333).each do |i|
+#   if i % 13563 == 0
+#     result << i
+#   end
+# end
+# p result
